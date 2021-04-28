0babfdcb918766f034e96bd8e06a6b29e4a25093
Gabriel g3 <gabrielfranccelino@gmail.com>
2021-04-28 14:51:00 -0300

    feat: link component header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,14 @@
+import Link from 'next/link';
+
 import styles from './header.module.scss';
 
 export default function Header() {
   return (
     <div className={styles.wrapper}>
       <div className={styles.container}>
-        <img src="/logo.svg" alt="logo" />
+        <Link href="/" passHref>
+          <img src="/logo.svg" alt="logo" />
+        </Link>
       </div>
     </div>
   );
